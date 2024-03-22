3647650364fc523a48254b10b72a93bda63b234b
issacemm <issacemanuel1122@gmail.com>
2024-03-22 10:47:23 -0400

    week4

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,12 +2,15 @@ import LoginPanel from "./components/Login/Login"
 import Register from "./components/Register/Register"
 
 import { Routes, Route } from "react-router-dom";
-
+import Dealers from './components/Dealers/Dealers.jsx';
 function App() {
   return (
     <Routes>
+      <Route path="/dealer/:id" element={<Dealers/>} />  
+      <Route path="/dealers" element={<Dealers/>} />
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register/>}/>
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
 
   );
